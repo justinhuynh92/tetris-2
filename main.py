4e4e0be8b03f5b6c4a94427a77c2b791d65e7678
Justin Huynh <justinhuynh639@gmail.com>
2023-11-06 14:58:14 -0800

    tetris 2

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame, sys
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Python Tetris")
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        pygame.quit()
+        sys.exit()
+    # Drawing
+    screen.fill(dark_blue)
+    pygame.display.update()
+    clock.tick(60)
