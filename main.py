b9dbc6ccd9bf74e70b8c71e36ab81b3ac63f8222
Justin Huynh <justinhuynh639@gmail.com>
2023-11-07 13:29:35 -0800

    started creating grid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from grid import Grid
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -7,6 +8,9 @@ screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
