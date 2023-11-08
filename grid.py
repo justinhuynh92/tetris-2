0e9b88efed93d9e94ff59028f704ee6dea48ee59
Justin Huynh <justinhuynh639@gmail.com>
2023-11-08 14:13:03 -0800

    changes

--- grid.py
+++ grid.py
@@ -4,9 +4,15 @@ class Grid:
         self.num_cols = 10
         self.cell_size = 30
         self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.colors = Colors.get_cell_colors()
 
     def print_grid(self):
         for row in range(self.num_rows):
             for column in range(self.num_cols):
                 print(self.grid[row][column], end = " ")
             print()
+
+    def is_inside(self, row, column):
+        if row >= 0 and row < self.num_rows and column >= 0 and column < self.num_cols:
+            return True
+        return False
